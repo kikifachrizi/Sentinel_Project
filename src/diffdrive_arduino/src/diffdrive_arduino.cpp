ccaaaa035f9c7a75814ce51d361ef84294cef9a4
kikifachrizi <kikifachrizi235@gmail.com>
2026-05-24 12:22:35 +0700

    udpate repo

---
+++ src/diffdrive_arduino/src/diffdrive_arduino.cpp
@@ -0,0 +1,108 @@
+#include "diffdrive_arduino/diffdrive_arduino.h"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+
+DiffDriveArduino::DiffDriveArduino()
+    : logger_(rclcpp::get_logger("DiffDriveArduino"))
+{}
+
+CallbackReturn DiffDriveArduino::on_init(const hardware_interface::HardwareInfo & info)
+{
+  if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS) {
+    return CallbackReturn::ERROR;
+  }
+
+  cfg_.left_wheel_name = info_.hardware_parameters["left_wheel_name"];
+  cfg_.right_wheel_name = info_.hardware_parameters["right_wheel_name"];
+  cfg_.loop_rate = std::stof(info_.hardware_parameters["loop_rate"]);
+  cfg_.device = info_.hardware_parameters["device"];
+  cfg_.baud_rate = std::stoi(info_.hardware_parameters["baud_rate"]);
+  cfg_.timeout = std::stoi(info_.hardware_parameters["timeout"]);
+  cfg_.enc_counts_per_rev = std::stoi(info_.hardware_parameters["enc_counts_per_rev"]);
+
+  l_wheel_.setup(cfg_.left_wheel_name, cfg_.enc_counts_per_rev);
+  r_wheel_.setup(cfg_.right_wheel_name, cfg_.enc_counts_per_rev);
+
+  RCLCPP_INFO(logger_, "Finished Configuration");
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn DiffDriveArduino::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  RCLCPP_INFO(logger_, "Configuring hardware...");
+  arduino_.setup(cfg_.device, cfg_.baud_rate, cfg_.timeout);
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn DiffDriveArduino::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  RCLCPP_INFO(logger_, "Starting Controller...");
+  arduino_.sendEmptyMsg();
+  arduino_.setPidValues(30, 20, 0, 100);
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn DiffDriveArduino::on_deactivate(const rclcpp_lifecycle::State & /*previous_state*/)
+{
+  RCLCPP_INFO(logger_, "Stopping Controller...");
+  return CallbackReturn::SUCCESS;
+}
+
+std::vector<hardware_interface::StateInterface> DiffDriveArduino::export_state_interfaces()
+{
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+  state_interfaces.emplace_back(l_wheel_.name, hardware_interface::HW_IF_VELOCITY, &l_wheel_.vel);
+  state_interfaces.emplace_back(l_wheel_.name, hardware_interface::HW_IF_POSITION, &l_wheel_.pos);
+  state_interfaces.emplace_back(r_wheel_.name, hardware_interface::HW_IF_VELOCITY, &r_wheel_.vel);
+  state_interfaces.emplace_back(r_wheel_.name, hardware_interface::HW_IF_POSITION, &r_wheel_.pos);
+  return state_interfaces;
+}
+
+std::vector<hardware_interface::CommandInterface> DiffDriveArduino::export_command_interfaces()
+{
+  std::vector<hardware_interface::CommandInterface> command_interfaces;
+  command_interfaces.emplace_back(l_wheel_.name, hardware_interface::HW_IF_VELOCITY, &l_wheel_.cmd);
+  command_interfaces.emplace_back(r_wheel_.name, hardware_interface::HW_IF_VELOCITY, &r_wheel_.cmd);
+  return command_interfaces;
+}
+
+return_type DiffDriveArduino::read(const rclcpp::Time & /*time*/, const rclcpp::Duration & period)
+{
+  if (!arduino_.connected()) {
+    return return_type::ERROR;
+  }
+
+  arduino_.readEncoderValues(l_wheel_.enc, r_wheel_.enc);
+
+  double deltaSeconds = period.seconds();
+
+  double pos_prev = l_wheel_.pos;
+  l_wheel_.pos = l_wheel_.calcEncAngle();
+  l_wheel_.vel = (l_wheel_.pos - pos_prev) / deltaSeconds;
+
+  pos_prev = r_wheel_.pos;
+  r_wheel_.pos = r_wheel_.calcEncAngle();
+  r_wheel_.vel = (r_wheel_.pos - pos_prev) / deltaSeconds;
+
+  return return_type::OK;
+}
+
+return_type DiffDriveArduino::write(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
+{
+  if (!arduino_.connected()) {
+    return return_type::ERROR;
+  }
+
+  arduino_.setMotorValues(
+    l_wheel_.cmd / l_wheel_.rads_per_count / cfg_.loop_rate,
+    r_wheel_.cmd / r_wheel_.rads_per_count / cfg_.loop_rate
+  );
+
+  return return_type::OK;
+}
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(
+  DiffDriveArduino,
+  hardware_interface::SystemInterface
+)
