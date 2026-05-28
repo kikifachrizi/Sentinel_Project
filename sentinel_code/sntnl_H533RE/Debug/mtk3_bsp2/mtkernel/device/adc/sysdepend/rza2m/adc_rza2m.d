02b349588bea81db574929dff99c702c6fc3411e
kiki legion <kikifachrizi235@gmail.com>
2026-05-28 10:49:20 +0700

    first ini docs directory

---
+++ sentinel_code/sntnl_H533RE/Debug/mtk3_bsp2/mtkernel/device/adc/sysdepend/rza2m/adc_rza2m.d
@@ -0,0 +1,8 @@
+mtk3_bsp2/mtkernel/device/adc/sysdepend/rza2m/adc_rza2m.o: \
+ ../mtk3_bsp2/mtkernel/device/adc/sysdepend/rza2m/adc_rza2m.c \
+ D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/machine.h \
+ D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/sysdepend/stm32_cube/nucleo_stm32h533/machine.h \
+ D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/sysdepend/stm32_cube/cpu/core/armv8m/machine.h
+D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/machine.h:
+D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/sysdepend/stm32_cube/nucleo_stm32h533/machine.h:
+D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/sysdepend/stm32_cube/cpu/core/armv8m/machine.h:
