2c5960e76286bdb322744a988e279782ed702af6
Rifqy Fachrizi <kikifachrizi235@gmail.com>
2026-05-28 11:56:13 +0700

    Revert "Docs"

--- sentinel_code/sntnl_H533RE/Debug/mtk3_bsp2/mtkernel/kernel/sysdepend/cpu/core/armv7a/cpu_cntl.d
+++
@@ -1,8 +0,0 @@
-mtk3_bsp2/mtkernel/kernel/sysdepend/cpu/core/armv7a/cpu_cntl.o: \
- ../mtk3_bsp2/mtkernel/kernel/sysdepend/cpu/core/armv7a/cpu_cntl.c \
- D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/machine.h \
- D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/sysdepend/stm32_cube/nucleo_stm32h533/machine.h \
- D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/sysdepend/stm32_cube/cpu/core/armv8m/machine.h
-D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/machine.h:
-D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/sysdepend/stm32_cube/nucleo_stm32h533/machine.h:
-D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/sysdepend/stm32_cube/cpu/core/armv8m/machine.h:
--- sentinel_code/sntnl_H533RE/Debug/mtk3_bsp2/mtkernel/lib/libtk/sysdepend/cpu/stm32h7/int_stm32h7.d
+++
@@ -1,8 +0,0 @@
-mtk3_bsp2/mtkernel/lib/libtk/sysdepend/cpu/stm32h7/int_stm32h7.o: \
- ../mtk3_bsp2/mtkernel/lib/libtk/sysdepend/cpu/stm32h7/int_stm32h7.c \
- D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/machine.h \
- D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/sysdepend/stm32_cube/nucleo_stm32h533/machine.h \
- D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/sysdepend/stm32_cube/cpu/core/armv8m/machine.h
-D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/machine.h:
-D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/sysdepend/stm32_cube/nucleo_stm32h533/machine.h:
-D:/Robotic/Sentinel/sentinel_code/sntnl_H533RE/mtk3_bsp2/include/sys/sysdepend/stm32_cube/cpu/core/armv8m/machine.h:
